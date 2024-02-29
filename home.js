b28a95a44e432b6565c01c0fc09dda5203149421
victorpaduati <victorpadua.ti@gmail.com>
2024-02-29 15:26:10 -0300

    Nova home finalizada

--- home.js
+++ home.js
@@ -1,3 +1,9 @@
 function mostraNome(nome) {
     return nome
 }
+
+function mostraSobreNome(sobreNome) {
+    return sobreNome;
+}
+
+//Finalizado o sistema da home
--- login.js
+++ login.js
@@ -2,3 +2,10 @@
 //Trabalhando no login
 
 //Finalizando sistema de login
+function login(nome, email) {
+    return 'Bem vindo';
+}
+
+
+//Sistema de login completo
+//Retornamos o nome do usuário quando logado
