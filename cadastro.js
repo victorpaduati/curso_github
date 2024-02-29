9c8d1621d27927dcd8b0728f6445f545908734b5
victorpaduati <victorpadua.ti@gmail.com>
2024-02-29 16:03:14 -0300

    Adicionando funcionalidades de mostrar a mensagem

--- cadastro.js
+++ cadastro.js
@@ -1,5 +1,12 @@
+
 function signUp(email) {
     return email;
 }
-
 //Pegando o email e cadastrando na plataforma.
+
+
+function mostraMensagem() {
+    return 'Bem vindo ao sistema';
+}
+
+//Adicionando função de mensagem.
